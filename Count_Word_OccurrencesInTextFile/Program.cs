28e4485965227106be70ee3cbb0b51621268c5f9
Dominik Chabera <dominikchabera@gmail.com>
2022-03-12 08:27:05 +0100

    Add project files.

---
+++ Count_Word_OccurrencesInTextFile/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Count_Word_OccurrencesInTextFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
